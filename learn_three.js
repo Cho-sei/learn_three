ae7a5a08d78d063ca864e6ae04ef4c19cecad0b0
ChoseiSha <knct0420@gmail.com>
2019-01-08 12:26:38 +0900

    No 2

--- learn_three.js
+++ learn_three.js
@@ -12,18 +12,23 @@ function init(){
 
 	const scene = new THREE.Scene();
 
-	const camera = new THREE.PerspectiveCamera(45, width / height);
+	const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
 	camera.position.set(0, 0, +1000);
 
-	const geometry = new THREE.BoxGeometry(300, 300, 300);
-	const material = new THREE.MeshNormalMaterial();
-	const box = new THREE.Mesh(geometry, material);
-	scene.add(box);
+	const geometry = new THREE.SphereGeometry(300, 30, 30);
+	const material = new THREE.MeshStandardMaterial({color: 0xFF0000});
+	const mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+
+	const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+	directionalLight.position.set(1, 1, 1);
+
+	scene.add(directionalLight);
 
 	tick();
 
 	function tick(){
-		box.rotation.y += 0.01;
+		mesh.rotation.y += 0.01;
 		renderer.render(scene, camera);
 
 		requestAnimationFrame(tick);
